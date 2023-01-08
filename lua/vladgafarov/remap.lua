78e8f28a6d3b32801f9c526fabd2aa66978d64a2
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-01-08 21:34:51 +0500

    remaps

--- lua/vladgafarov/remap.lua
+++ lua/vladgafarov/remap.lua
@@ -1,3 +1,25 @@
-
 vim.g.mapleader = " "
+
+-- insert mode
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
+
+-- normal mode
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+
+-- visual block mode
+vim.keymap.set("x", "<A-j>", ":m '>+1<CR>gv-gv")
+vim.keymap.set("x", "<A-k>", ":m '>-2<CR>gv-gv")
+
+-- terminal mode
+vim.keymap.set("t", "<C-h>", "<C-\\><C-N><C-w>h")
+vim.keymap.set("t", "<C-j>", "<C-\\><C-N><C-w>j")
+vim.keymap.set("t", "<C-k>", "<C-\\><C-N><C-w>k")
+vim.keymap.set("t", "<C-l>", "<C-\\><C-N><C-w>l")
+
