e36a07a98701e57cbb63befb5c3ffa7e1bff5e1c
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-01-08 22:52:53 +0500

    rename and configure nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -8,5 +8,9 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup{
+	filters = {
+		custom = {"^.git$"}
+	}
+}
 
