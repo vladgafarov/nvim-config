35317544acd5a4ca5974af241249ab9ba28ae42c
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-03-05 21:53:22 +0500

    create theme config file

--- lua/vladgafarov/packer.lua
+++ lua/vladgafarov/packer.lua
@@ -2,18 +2,12 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-
-	use {
-		'nvim-telescope/telescope.nvim', branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-
-	use {'dracula/vim', as = 'dracula',
-	config = function()
-		vim.cmd('colorscheme dracula')
-	end
+use 'wbthomason/packer.nvim'
+use {
+	'nvim-telescope/telescope.nvim', branch = '0.1.x',
+	requires = { {'nvim-lua/plenary.nvim'} }
 }
+use {'dracula/vim', as = 'dracula'}
 use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 use('mbbill/undotree')
 use {
