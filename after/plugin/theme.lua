1bbf428cd711b027fe0d3a4718e5b407e9a2e730
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-03-05 22:10:35 +0500

    install some themes

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,1 +1,14 @@
+vim.cmd('set termguicolors')
+
+--vim.cmd('let ayucolor="light"')
+--vim.cmd('let ayucolor="mirage"')
+--vim.cmd('let ayucolor="dark"')
+--vim.cmd('colorscheme ayu')
+
+--vim.cmd('colorscheme night-owl')
+
 vim.cmd('colorscheme dracula')
+
+--vim.cmd('colorscheme iceberg')
+
+--vim.cmd('colorscheme one')
