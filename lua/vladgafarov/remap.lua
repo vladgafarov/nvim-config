c4afb524a5af6589091909b6403e6c4ecc7d7173
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-03-05 23:02:31 +0500

    comment in visual mode

--- lua/vladgafarov/remap.lua
+++ lua/vladgafarov/remap.lua
@@ -35,4 +35,7 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, {noremap = true, sile
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, {noremap = true, silent = true})
 vim.keymap.set('n', '<space>d', 'v:lua.vim.lsp.omnifunc', {noremap = true, silent = true})
 
+-- comment in normal mode
 vim.keymap.set("n", "<C-_>", ':call nerdcommenter#Comment(0,"toggle")<CR>', {noremap = true, silent = true})
+-- comment in visual line mode 
+vim.keymap.set("x", "<C-_>", ':call nerdcommenter#Comment(0,"toggle")<CR>', {noremap = true, silent = true})
