b04706124a4521dcc723f128364c31aa8f7e300b
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-04-05 14:04:29 +0500

    add dracula pro theme

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,5 +1,18 @@
 vim.cmd('set termguicolors')
 
+vim.cmd('let g:dracula_colorterm = 1')
+vim.cmd('let g:dracula_bold = 1')
+vim.cmd('let g:dracula_italic = 1')
+vim.cmd('let g:dracula_underline = 1')
+vim.cmd('let g:dracula_undercurl = 1')
+vim.cmd('colorscheme dracula_pro_lincoln')
+vim.cmd('colorscheme dracula_pro_base')
+vim.cmd('colorscheme dracula_pro_blade')
+vim.cmd('colorscheme dracula_pro_morbius')
+vim.cmd('colorscheme dracula_pro_buffy')
+vim.cmd('colorscheme dracula_pro')
+vim.cmd('colorscheme dracula_pro_van_helsing')
+
 --vim.cmd('let ayucolor="light"')
 --vim.cmd('let ayucolor="mirage"')
 --vim.cmd('let ayucolor="dark"')
@@ -7,7 +20,7 @@ vim.cmd('set termguicolors')
 
 --vim.cmd('colorscheme night-owl')
 
-vim.cmd('colorscheme dracula')
+--vim.cmd('colorscheme dracula')
 
 --vim.cmd('colorscheme iceberg')
 
