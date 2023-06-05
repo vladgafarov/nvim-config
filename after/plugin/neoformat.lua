99dcf7cf1247e5c141ee6d5807b72c06d6542993
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-06-05 21:29:45 +0500

    update neoformat config

--- after/plugin/neoformat.lua
+++ after/plugin/neoformat.lua
@@ -1,5 +1,5 @@
 vim.cmd('let g:neoformat_try_node_exe = 1')
-vim.cmd('autocmd BufWritePre *.{js,ts,jsx,tsx,css,sass,py,json} Neoformat')
+vim.cmd('autocmd BufWritePre * undojoin | Neoformat')
 --vim.cmd('autocmd BufWritePre,TextChanged,InsertLeave *.{js,ts,jsx,tsx,css,sass} Neoformat')
 
 
